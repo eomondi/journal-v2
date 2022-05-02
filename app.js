61579296cfcabe95cc9efd49b20c92feb813a31e
Emmanuel <59773467+eomondi@users.noreply.github.com>
2022-05-03 01:00:42 +0300

    Complete v1

--- app.js
+++ app.js
@@ -1,30 +1,87 @@
 const express = require("express");
 const ejs = require("ejs");
+const _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Write and see your journal entries by clicking 'write something' in the menu or at the footer. Anything you write will be seen by everyone. Likewise, everyone can erase what you write. If you wish to erase your journal entries, scroll to the footer of any page and click the 'Erase Journal' button. (Keeping a journal allows you to record what’s happening in your life and to work through your thoughts and feelings. Sometimes, you might write a journal for school to help you deepen your understanding of what you’re studying. Fortunately, writing a journal entry is a simple process. First, choose a topic to write about, like what's happening in your life. Then, write an opening for your entry and express your thoughts. - WikiHow)";
+
+const aboutContent = "About me*. My name is at the footer. I have other projects that work similar to this one (especially the To Do List). If you are interested in checking them out, click either of the buttons below.";
+
+const contactContent = "Contact me*. I am not yet open for business as a full stack developer. However, you can follow me on Twitter (@elomondi) to track my progress. I will consider a serious offer once I complete learning everything I need to know. I am currently interested in mentorship, accountability, feedback, peers and career support. If you are willing and able to freely offer those, you could DM me so that we talk about it.";
+
+let posts = [];
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.use(express.static("public"));
 
+app.get('/', function(req, res) {
 
+  res.render("home", {
+    homeWords: homeStartingContent,
+    morePosts: posts
+  });
 
+});
 
+app.get('/about', function(req, res) {
 
+  res.render("about", {
+    aboutWords: aboutContent
+  });
 
+});
 
+app.get('/contact', function(req, res) {
 
+  res.render("contact", {
+    contactWords: contactContent
+  });
 
+});
 
+app.get('/compose', function(req, res) {
 
+  res.render("compose");
 
+});
 
+app.post('/compose', function(req, res) {
+
+  const blogPost = {
+    blogTitle: req.body.postTitle,
+    blogBody: req.body.postBody
+  };
+
+  posts.push(blogPost);
+
+  res.redirect('/');
+
+});
+
+app.get('/posts/:postName', function(req, res) {
+  for (var i = 0; i < posts.length; i++) {
+    if (_.lowerCase(posts[i].blogTitle) === _.lowerCase(req.params.postName)) {
+      res.render("post", {
+        requestedTitle: _.startCase(_.toLower(posts[i].blogTitle)),
+        requestedPost: posts[i].blogBody
+      });
+    }
+  }
+});
+
+app.get('/erase', function(req, res) {
+  posts = [];
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT || 3000, function() {
+
   console.log("Server started on port 3000.");
+
 });
